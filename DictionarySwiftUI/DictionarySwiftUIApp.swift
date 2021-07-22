ee5d50e18f41cbf16a14549ee3f5b735450bd998
vagarkov <vagarkov@oggettoweb.com>
2021-07-22 20:17:54 +0300

    initial

---
+++ DictionarySwiftUI/DictionarySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionarySwiftUIApp.swift
+//  DictionarySwiftUI
+//
+//  Created by Vyacheslav Agarkov on 22.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct DictionarySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
